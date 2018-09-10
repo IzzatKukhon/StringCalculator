b5e9e43a3d510307af6076a9e8bc33372cbbf9bc
Izzat Kukhon <eng_3zat@hotmail.com>
2018-09-10 15:09:50 -0500

    Add the Calculator class

---
+++ StringCalculator/Caluculator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Caluculator
+    {
+        public Caluculator()
+        {
+
+        }
+
+        public static Int64 Add(string numbers)
+        {
+
+            List<string> delemiters = new List<string> { ",", "\n" };
+            Int64 result = 0;
+            char newDelem;
+
+            string newDelecmStr = string.Empty;
+            if (string.IsNullOrEmpty(numbers))
+                return 0;
+
+            if (numbers.StartsWith("//") && numbers.Contains("\n"))
+            {
+
+                numbers = numbers.TrimStart('/');
+                string newDelems = numbers.Substring(0, numbers.IndexOf('\n'));
+                numbers = numbers.Substring(numbers.IndexOf('\n') + 1);
+
+                Regex regex = new Regex(@"\[(?<delem>.*?)\]");
+                MatchCollection matches = regex.Matches(newDelems.TrimEnd('\n'));
+
+                if (!string.IsNullOrEmpty(newDelems) &&  newDelems.Length == 1)
+                    delemiters.Add(newDelems);
+
+                else if(!string.IsNullOrEmpty(newDelems) && newDelems.Length > 1)
+                {
+                    foreach (Match _match in matches)
+                    {
+                        delemiters.Add(_match.Groups["delem"].Value);
+                    }
+                }
+                //string[] DelemAndNumbers = numbers.Split('\n');
+                //
+                //newDelem = numbers[0];
+                //
+                //newDelecmStr = Convert.ToString(newDelem);
+                
+            }
+
+            string[] numberstoAdd = numbers.Split(delemiters.ToArray<string>(), StringSplitOptions.RemoveEmptyEntries);
+
+            if (numberstoAdd != null && numberstoAdd.Count() == 0)
+                return 0;
+
+            var negatives = numberstoAdd.ToList<string>().Where(num => Convert.ToInt64(num) < 0).ToList();
+            if(negatives != null && negatives.Count > 0)
+                throw new Exception(string.Format("negatives not allowed: ({0})", string.Join(", ", negatives)));
+
+            foreach (string _str in numberstoAdd)
+            {
+                Int64 _num = Convert.ToInt64(_str);
+
+                if (_num < 0)
+                    throw new Exception(string.Format("negatives not allowed - {0}", _num));
+
+                if (_num > 1000)
+                    continue;
+
+                result += _num;               
+            }
+
+            return Convert.ToInt64(result);
+        }
+    }
+}
--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -10,6 +10,28 @@ namespace StringCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"\")", Caluculator.Add("")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"1,2,1001\")", Caluculator.Add("1,2,1001")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"1,2,3,1005\")", Caluculator.Add("1,2,3,1005")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"1,2\\n3\")", Caluculator.Add("1,2\n3")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"1\\n2,3\")", Caluculator.Add("1\n2,3")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//;\\n1\\n2,3;4\")", Caluculator.Add("//;\n1\n2,3;4")));
+
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//y\\n1\\n2y3\\n4,9y0\")", Caluculator.Add("//y\n1\n2y3\n4,9y0")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//;\\n1;2;7;7\")", Caluculator.Add("//;\n1;2;7;7")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//[*][%]\\n1*2%3*1200\")", Caluculator.Add("//[*][%]\n1*2%3*1200")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//[*][%]\\n1**2%3\")", Caluculator.Add("//[*][%]\n1**2%3")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//[*][%][_]\\n1*2%%3___5%%1002\")", Caluculator.Add("//[*][%][_]\n1*2%%3___5%%1002")));
+            Console.WriteLine(string.Format("{0} = {1}", "Caluculator.Add(\"//[**][%%][__][&]\\n1**2%%3__5&9\")", Caluculator.Add("//[**][%%][__][&]\n1**2%%3__5&9")));
+           
+            //Console.WriteLine(Caluculator.Add("//y\n1\n2y3\n4,-9y-6"));
+
+            //Console.WriteLine(Caluculator.Add("//[y][x]\n1\n2y3\n4,-9x-6"));
+ 
+
+
+
+            Console.ReadLine();
         }
     }
 }
